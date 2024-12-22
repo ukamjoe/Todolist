7615001971d896265959632052ef8587bb1dd0a3
UKAM JOSEPH AGBANIMUSHUYE <josephukam71@gmail.com>
2024-12-22 16:45:33 +0100

    main

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -1,22 +1,31 @@
-// src/components/ErrorBoundary.js
 import React from "react";
+import PropTypes from "prop-types";
 
 class ErrorBoundary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
 
-    static getDerivedStateFromError() {
-        return { hasError: true };
-    }
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
 
-    render() {
-        if (this.state.hasError) {
-            return <h1>Something went wrong.</h1>;
-        }
-        return this.props.children;
+  componentDidCatch(error, errorInfo) {
+    console.error("Error:", error);
+    console.error("Error Info:", errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
     }
+    return this.props.children;
+  }
 }
+
+ErrorBoundary.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default ErrorBoundary;
