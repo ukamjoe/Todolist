6ff3714287eda0d42125b1a8cfc584be3335c0d8
UKAM JOSEPH AGBANIMUSHUYE <josephukam71@gmail.com>
2024-12-22 17:34:02 +0100

    main

--- src/Errorpage.jsx
+++ src/Errorpage.jsx
@@ -1,13 +1,24 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React from 'react';
+import jacky from './images/jacky.jpeg'; // Corrected the image path
 
 const ErrorPage = () => {
   return (
-    <div className="page">
-        <h1>404 - Page Not Found</h1>
-        <p>Sorry, the page you are looking for does not exist.</p>
+    <div className="page" style={{ textAlign: 'center', padding: '20px' }}>
+      {/* Displaying the image */}
+      <img 
+        src={jacky} 
+        alt="Jacky" 
+        style={{ width: '225px', height: '225px', borderRadius: '50%', marginBottom: '20px' }} 
+      />
+      
+      {/* Error Message */}
+      <h1 style={{ color: 'red', fontSize: '24px' }}>404 - Page Not Found</h1>
+      <p style={{ color: 'gray', fontSize: '18px' }}>
+        Sorry, the page you are looking for does not exist.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
