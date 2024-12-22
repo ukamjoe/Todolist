297b4cf7f7f89a0b8f3881dd73ef89df48bb43dc
UKAM JOSEPH AGBANIMUSHUYE <josephukam71@gmail.com>
2024-12-22 23:59:36 +0100

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import TodoList from "./Todolist.jsx";
 import { Route, Routes } from "react-router-dom";
-import ErrorPage from "./ErrorPage.jsx"
+import ErrorPage from "./Errorpage.jsx";
 import ErrorBoundary from "./ErrorBoundary.jsx";
 
 const App = () => {
